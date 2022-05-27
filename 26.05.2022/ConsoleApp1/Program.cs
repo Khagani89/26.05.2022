6b55e08c5387fe3dd46d1c0d6929953b0a1254ce
Khagani89 <khaganichh@code.edu.az>
2022-05-27 12:23:12 +0400

    Project

---
+++ 26.05.2022/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] reqemler = { 6, 7, 8, 9 };
+            
+            reqemler = Metod(reqemler, 20);
+        }
+
+        static int Metod( int[] nums, int a)
+        {
+         
+            int[] newnums = new int[nums.Length + 1];
+            {
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    newnums[i] = nums[i];
+                }
+                    newnums[newnums.Length - 1] = a;
+
+            return newnums;
+            }
+            
+        }             
+        
+        
+            
+            
+            
+        
+
+            
+
+        
+    }
+}
